732f61b39d9e04f710d78d480da21a2459879e3e
l0ss <mike@mikeloss.net>
2021-09-13 12:56:34 +0000

    Packages done!

--- Group3r/Assessment/Analysers/Package.cs
+++ Group3r/Assessment/Analysers/Package.cs
@@ -13,6 +13,61 @@ namespace Group3r.Assessment.Analysers
         public override SettingResult Analyse(AssessmentOptions assessmentOptions)
         {
             List<GpoFinding> findings = new List<GpoFinding>();
+
+            PathAnalyser pathAnalyser = new PathAnalyser(assessmentOptions);
+
+            foreach (string msiPath in setting.MsiFileList)
+            {
+                if (!String.IsNullOrWhiteSpace(msiPath))
+                {
+                    if (msiPath.StartsWith("\\\\"))
+                    {
+                        // shortcut targets a network share, we need to look at that
+                        PathFinding pathFinding = pathAnalyser.AnalysePath(msiPath);
+
+                        if (pathFinding != null)
+                        {
+                            if (pathFinding.FileExists && pathFinding.FileWritable)
+                            {
+                                if ((int)this.MinTriage < 4)
+                                {
+                                    findings.Add(new GpoFinding()
+                                    {
+                                        FindingReason = "MSI package installer setting points at a file that you can modify.",
+                                        FindingDetail = "It points to " + msiPath + " so maybe see what happens if you replace that file with something fun.",
+                                        Triage = Constants.Triage.Red
+                                    });
+                                }
+                            }
+                            else if (!pathFinding.FileExists && pathFinding.DirectoryExists && pathFinding.DirectoryWritable)
+                            {
+                                if ((int)this.MinTriage < 4)
+                                {
+                                    findings.Add(new GpoFinding()
+                                    {
+                                        FindingReason = "MSI package installer points to a file that doesn't exist, in a directory that you can write to.",
+                                        FindingDetail = "It points to " + msiPath + " so maybe see what happens if you put something fun in there.",
+                                        Triage = Constants.Triage.Red
+                                    });
+                                }
+                            }
+                            else if (!pathFinding.FileExists && !pathFinding.DirectoryExists && !String.IsNullOrWhiteSpace(pathFinding.ParentDirectoryExists) && pathFinding.ParentDirectoryWritable)
+                            {
+                                if ((int)this.MinTriage < 4)
+                                {
+                                    findings.Add(new GpoFinding()
+                                    {
+                                        FindingReason = "MSI package installer points to a file that doesn't exist, in a directory that ALSO doesn't exist, but there's a parent directory that DOES exist that you can write to.",
+                                        FindingDetail = "It points to " + msiPath + " so maybe see what happens if you create that file.",
+                                        Triage = Constants.Triage.Red
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             /*
             findings.Add(new GpoFinding()
             {
@@ -27,6 +82,7 @@ namespace Group3r.Assessment.Analysers
             // make a new setting object minus the ugly bits we don't care about.
             SettingResult.Setting = new PackageSetting();
             */
+            SettingResult.Findings = findings;
             SettingResult.Setting = setting;
 
             return SettingResult;
